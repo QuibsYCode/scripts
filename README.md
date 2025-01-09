1943e75dc2008c91d3e201fc72290d5518a474f4
QuibsYCode <86725890+QuibsYCode@users.noreply.github.com>
2025-01-09 12:30:14 -0600

    Umbrella Domain Check

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# scripts
+# Scripts
+Some scripts I've created for use at work. For problems that no one else had but, probably.
+
+## Umbrella Domain Check
+This script can be used to generate your API token and fetch the results of requests, allows, and blocks for the last 30 days across your organizations. I wanted to have a quick way to resolve DNS checking when vendors provide a list of URLs to whitelist, or quickly check the status of a domain. More work can be done on this in the future, such as adding categorization for the domains or determining the allow/blocklist that it is on (if any).
---
+++ Umbrella Domain Check.py
@@ -0,0 +1,72 @@
+import requests
+import base64
+
+token_url = "https://api.umbrella.com/auth/v2/token"
+client_id = "YOUR_CLIENT_ID"
+client_secret =  "YOUR_CLIENT_SECRET"
+orgID = [] # List of your organizations, if applicable
+
+domains = "" # Add domains to check as a comma separated list, ex: google.com,youtube.com
+
+credentials = f"{client_id}:{client_secret}"
+access_token = base64.b64encode(credentials.encode()).decode()
+
+for organization in orgID:
+    # Set the organization name to human readable text
+    orgName = ""
+    if organization == orgID[0]:
+            orgName = "ORG NAME"
+    elif organization == orgID[1]:
+            orgName = "ORG NAME"
+    elif organization == orgID[2]:
+            orgName = "ORG NAME"
+    else:
+        orgName = "Not Found"
+    
+    # Post the access token for the specific organization and retrieve the bearer token
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-Umbrella-OrgId': organization,
+        'Authorization': f"Basic {access_token}",
+    }
+    token_url = "https://api.umbrella.com/auth/v2/token"
+    response = requests.post(token_url, headers=headers)
+    response_data = response.json()
+    bearer_token = response_data.get("access_token")
+
+    # Form the request, setting for 30 day histry by default
+    headers = {
+        'Authorization': f"Bearer {bearer_token}",
+    }
+    params = {
+        'to': 'now',
+        'from': '-30days',
+        'domains': domains,
+    }
+    url = f"https://reports.api.umbrella.com/v2/organizations/{organization}/summaries-by-destination/dns"
+    response = requests.get(url, params=params, headers=headers)
+
+    # Return the results, formatted as I like 
+    if response.status_code == 200:
+        print()
+        print(f"Organization: {orgName}")
+        response_data = response.json()
+        for item in response_data['data']:
+            domain = item['domain']
+            request = item['summary']['requests']
+            requestallow = item['summary']['requestsallowed']
+            requestblock = item['summary']['requestsblocked']
+            if requestallow > 0 and requestblock == 0:
+                domaincheck = f"{domain}: Allowed."
+            if requestallow == 0 and requestblock > 0:
+                domaincheck = f"{domain}: Blocked."
+            if requestallow == 0 and requestblock == 0:
+                domaincheck = f"{domain}: Selectively Proxied."
+            print()
+            print(f"\t{domaincheck}")
+            print(f"\t\tRequest Count: {request}")
+            print(f"\t\tRequests Allowed: {requestallow}")
+            print(f"\t\tRequests Blocked: {requestblock}")
+            print()
+    else:
+        print(f"Error {response.status_code}: {response.text}")
